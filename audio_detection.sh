39850c26831a32d9ece06f83e6e5b405a22ef1ca
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-09-22 19:16:50 +0900

    Update audio_detection.sh

--- audio_detection.sh
+++ audio_detection.sh
@@ -1,11 +1,23 @@
+FILE = "/home/audio_detection/Cnn14_mAP=0.431.pth"
+if [ ! -e $FILE ];then
+    wget -O "Cnn14_mAP=0.431.pth" https://zenodo.org/record/3987831/files/Cnn14_mAP%3D0.431.pth?download=1
+fi
 
+
+SECONDS=0
 /usr/local/bin/python3 /home/audio_detection/scripts/test.py 
 
-wget -O "Cnn14_mAP=0.431.pth" https://zenodo.org/record/3987831/files/Cnn14_mAP%3D0.431.pth?download=1
+while [True]
 
-CUDA_VISIBLE_DEVICES=0 /usr/local/bin/python3 /home/audio_detection/pytorch/inference.py audio_tagging \
-    --model_type="Cnn14" \
-    --checkpoint_path="Cnn14_mAP=0.431.pth" \
-    --audio_path="/home/audio_detection/resources/test.wav" \
-    --cuda
+do  
+    CUDA_VISIBLE_DEVICES=0 /usr/local/bin/python3 /home/audio_detection/pytorch/inference.py audio_tagging \
+        --model_type="Cnn14" \
+        --checkpoint_path="Cnn14_mAP=0.431.pth" \
+        --audio_path="/home/audio_detection/resources/test.wav" \
+        --cuda
+        
+   
+   if [$SECONDS -gt 110];then
+       /usr/local/bin/python3 /home/audio_detection/scripts/test.py
+       SECONDS=0
 
