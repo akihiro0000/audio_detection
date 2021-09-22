504e03a5454d387650e52fb2bd2577e432255968
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-09-22 18:50:57 +0900

    Update audio_detection.sh

--- audio_detection.sh
+++ audio_detection.sh
@@ -1,7 +1,7 @@
 
 /usr/local/bin/python3 /home/audio_detection/scripts/test.py 
 
-wget -O $CHECKPOINT_PATH https://zenodo.org/record/3987831/files/Cnn14_mAP%3D0.431.pth?download=1
+wget -O "Cnn14_mAP=0.431.pth" https://zenodo.org/record/3987831/files/Cnn14_mAP%3D0.431.pth?download=1
 
 CUDA_VISIBLE_DEVICES=0 /usr/local/bin/python3 /home/audio_detection/pytorch/inference.py audio_tagging \
     --model_type="Cnn14" \
