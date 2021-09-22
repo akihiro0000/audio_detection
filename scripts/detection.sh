884c853d56687e5aab4280923ca4d7f7fd1c4d7d
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-09-22 18:28:28 +0900

    Delete detection.sh

--- scripts/detection.sh
+++
@@ -1,8 +0,0 @@
-
-/usr/local/bin/python3 /home/audio_detection/scripts/test.py 
-CUDA_VISIBLE_DEVICES=0 /usr/local/bin/python3python3 /home/audio_detection/pytorch/inference.py audio_tagging \
-    --model_type="Cnn14" \
-    --checkpoint_path="Cnn14_mAP=0.431.pth" \
-    --audio_path="/home/audio_detection/resources/test.wav" \
-    --cuda
-
