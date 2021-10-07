8af60c06496ced8dbfcc8bf15916c6c13294c8e1
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-10-07 17:20:40 +0900

    Create test_audio_detection.sh

---
+++ scripts/test_audio_detection.sh
@@ -0,0 +1,12 @@
+while :
+
+do  
+    /usr/local/bin/python3 /home/audio_detection/scripts/record_data.py 
+    
+    CUDA_VISIBLE_DEVICES=0 /usr/local/bin/python3 /home/audio_detection/pytorch/inference.py audio_tagging \
+        --model_type="Cnn14" \
+        --checkpoint_path="Cnn14_mAP=0.431.pth" \
+        --audio_path="/home/audio_detection/resources/test.wav" \
+        --cuda
+        
+done
