4a20757fff74e57f81e60f14d391b3ea164ba5be
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-10-07 17:18:20 +0900

    Create record_data.py

---
+++ scripts/record_data.py
@@ -0,0 +1,35 @@
+import pyaudio
+import wave
+
+form_1 = pyaudio.paInt16 # 16-bit resolution
+chans = 1 # 1 channel
+samp_rate = 44100 # 44.1kHz　サンプリング周波数
+chunk = 128 # 2^12 一度に取得するデータ数
+record_secs = 8 # 録音する秒数
+dev_index = 24 # デバイス番号
+wav_output_filename = './resources/test.wav' # 出力するファイル
+
+
+audio = pyaudio.PyAudio()
+
+stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                input_device_index = dev_index,input = True, \
+                frames_per_buffer=chunk)
+frames = []
+
+for i in range(0,int((samp_rate/chunk)*record_secs)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+print("finished recording")
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+wavefile = wave.open(wav_output_filename,'wb')
+wavefile.setnchannels(chans)
+wavefile.setsampwidth(audio.get_sample_size(form_1))
+wavefile.setframerate(samp_rate)
+wavefile.writeframes(b''.join(frames))
+wavefile.close()
