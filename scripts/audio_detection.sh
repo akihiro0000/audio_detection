c917e1d5a7c1571b51cf7fa128ff47f12914fa72
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-10-12 15:24:50 +0900

    Update audio_detection.sh

--- scripts/audio_detection.sh
+++ scripts/audio_detection.sh
@@ -3,8 +3,6 @@ while :
 do  
     /usr/local/bin/python3 /home/audio_detection/scripts/record_data.py > /dev/null 2>&1
     
-    clear
-    
     CUDA_VISIBLE_DEVICES=0 /usr/local/bin/python3 /home/audio_detection/pytorch/inference.py audio_tagging \
         --model_type="Cnn14" \
         --checkpoint_path="Cnn14_mAP=0.431.pth" \
